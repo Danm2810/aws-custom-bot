7c838944d17605b1b0c36beee90b68be927dc71f
Daniel Mikhail <153936090+Danm2810@users.noreply.github.com>
2024-09-12 11:38:47 -0400

    Create Node.js lambda function

---
+++ Node.js
@@ -0,0 +1,50 @@
+/*******************************************************************************************************
+
+The following code is used to configure a Lambda funciton for PizzaOrderProcessorRole.. 
+All of this is done within AWS
+
+*******************************************************************************************************/
+
+'use strict';
+     
+// Close dialog with the customer, reporting fulfillmentState of Failed or Fulfilled ("Thanks, your pizza will arrive in 20 minutes")
+function close(sessionAttributes, fulfillmentState, message) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Close',
+            fulfillmentState,
+            message,
+        },
+    };
+}
+ 
+// --------------- Events -----------------------
+ 
+function dispatch(intentRequest, callback) {
+    console.log(`request received for userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.name}`);
+    const sessionAttributes = intentRequest.sessionAttributes;
+    const slots = intentRequest.currentIntent.slots;
+    const crust = slots.crust;
+    const size = slots.size;
+    const pizzaKind = slots.pizzaKind;
+    
+    callback(close(sessionAttributes, 'Fulfilled',
+    {'contentType': 'PlainText', 'content': `Okay, I have ordered your ${size} ${pizzaKind} pizza on ${crust} crust`}));
+    
+}
+ 
+// --------------- Main handler -----------------------
+ 
+// Route the incoming request based on intent.
+// The JSON body of the request is provided in the event slot.
+export const handler = (event, context, callback) => {
+    try {
+        dispatch(event,
+            (response) => {
+                callback(null, response);
+            });
+    } catch (err) {
+        callback(err);
+    }
+};
